aac2257ab034ac342f500cadde7afa2ee239bd17
levmetal <levi.oquendo@gmail.com>
2024-09-21 21:21:46 -0400

    connectedplayersound

--- index.js
+++ index.js
@@ -1,40 +1,44 @@
 import express from 'express';
 import axios from 'axios';
-import path from 'path';  // Para manejar rutas de archivos
-const port = 3000;
+import path from 'path';
+import cors from 'cors';  // Asegúrate de importar cors
+
+const port = 4000;
 const app = express();
 
+// Middleware CORS
+app.use(cors());
+
 // Ruta para servir archivos estáticos (HTML, CSS, JS, etc.)
-const __dirname = path.resolve();  // Obtener el directorio actual (compatible con ES Modules)
-app.use(express.static(path.join(__dirname, 'public')));  // Servir la carpeta 'public'
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Ruta para obtener el audio desde inv.nadeko.net
 app.get('/audio', async (req, res) => {
   const videoId = req.query.id;
+  
 
   if (!videoId) {
-      return res.status(400).send('Missing video ID');
+    return res.status(400).send('Missing video ID');
   }
 
   try {
-      const baseUrl = `https://inv.nadeko.net/latest_version?id=${videoId}&itag=18&local=true`;
+    const baseUrl = `https://inv.nadeko.net/latest_version?id=${videoId}&itag=18&local=true`;
 
-      const response = await axios.get(baseUrl, {
-          maxRedirects: 0,
-          validateStatus: function (status) {
-              return status >= 300 && status < 400;  // Solo redirecciones
-          }
-      });
+    const response = await axios.get(baseUrl, {
+      maxRedirects: 0,
+      validateStatus: function (status) {
+        return status >= 300 && status < 400;
+      }
+    });
 
-      // Obtener la URL de redirección
-      const relativeUrl = response.headers.location;
-      const finalUrl = `https://inv.nadeko.net${relativeUrl}`;  // Agregar el dominio
+    const relativeUrl = response.headers.location;
+    const finalUrl = `https://inv.nadeko.net${relativeUrl}`;
 
-      res.json({ audioUrl: finalUrl });
-
+    res.json({ audioUrl: finalUrl });
   } catch (error) {
-      console.error('Error al obtener la URL de redirección:', error);
-      res.status(500).send('Error al obtener la URL de redirección');
+    console.error('Error al obtener la URL de redirección:', error);
+    res.status(500).send('Error al obtener la URL de redirección');
   }
 });
 
